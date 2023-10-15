8d5f63eca25062b0b72b401e543ee33d5a39a00e
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2023-10-15 17:03:35 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5001
+const cors = require('cors')
 
 const chef = require('./data/chef.json');
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Dragon is running')
