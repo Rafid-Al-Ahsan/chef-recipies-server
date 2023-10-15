1f2fc9c171d989f24fe0e1f0db459f5f87a6e0bb
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2023-10-15 16:30:47 +0600

    path to chef data added

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5001;
 
+const chef = require('./data/chef.json');
+
 app.get('/', (req, res) => {
     res.send('Dragon is running')
 })
 
+
+app.get('/chef', (req,res) => {
+    res.send(chef)
+})
+
 app.listen(port, ()=> {
+    console.log(`The API is running on port: ${port}`);
 })
